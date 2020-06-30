98fe84dd691440b0421d811d7ad7af2b78a40948
Chip Hughes <chiphughes@verizon.net>
2020-06-30 18:20:11 -0400

    Navbar added.

--- LearningDjango/urls.py
+++ LearningDjango/urls.py
@@ -26,5 +26,6 @@ import HelloDjangoApp.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', HelloDjangoApp.views.index, name='index'),
-    url(r'^home$', HelloDjangoApp.views.index, name='home')
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
 ]
